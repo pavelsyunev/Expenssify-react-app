caf35099dacb2424192767c96cb0a826177f8782
Pavel Syunev <pavel.syunev@gmail.com>
2020-03-23 19:52:34 -0400

    Format amound data

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import numeral from "numeral";
 
 const ExpenseListItem = props => (
   <div>
     <Link to={`/edit/${props.id}`}>
       <h3>{props.description}</h3>
     </Link>
-    <p>{props.amount} 💲</p>
+    <p>{numeral(props.amount).format("0,0[.]00 $")}</p>
     <p>{props.createdAt.toString()}</p>
   </div>
 );
